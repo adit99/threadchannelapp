d912686debd3b2a58c1feb95fc56f5a8d28a101f
Aditya Jayaraman <adit99@example.com>
2015-03-02 22:52:26 -0800

    initial commit

---
+++ threadchannelapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  threadchannelapp
+//
+//  Created by Aditya Jayaraman on 3/2/15.
+//  Copyright (c) 2015 threadchannel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
